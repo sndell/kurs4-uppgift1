6cbe14e4a2273d8a28b20f4b9f43c516d66b7525
sndell <sundell.benjamin@gmail.com>
2023-01-05 22:57:44 +0100

    changes

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,20 @@
 let products;
 
 addEventListener('load', async () => {
+  const local = JSON.parse(localStorage.getItem('products'));
+  if (local) {
+    generateProducts(local);
+    return;
+  }
+
   const res = await fetch('https://fakestoreapi.com/products');
   const data = await res.json();
-  products = data;
+  localStorage.setItem('products', JSON.stringify(data));
+  generateProducts(data);
 });
+
+generateProducts = (products) => {
+  products.forEach((product) => {
+    // console.log(product);
+  });
+};
